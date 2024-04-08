cefc5b9bf3432b7834c36eeab27c3b720935b368
rahulp18 <pradhanrahul3346@gmail.com>
2024-04-08 09:48:21 +0530

    Error handleing

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -17,10 +17,14 @@ export class ArticlesService {
     return this.prisma.article.findMany({ where: { published: false } });
   }
 
-  findOne(id: number) {
-    return this.prisma.article.findUnique({
+  async findOne(id: number) {
+    const article = await this.prisma.article.findUnique({
       where: { id },
     });
+    if (!article) {
+      return new NotFoundException(`Invalid id`);
+    }
+    return article;
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
